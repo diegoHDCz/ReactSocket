54e63ee0aaf1ee23b476312efdb3511cd784cef5
Czajka <dncj@gft.com>
2024-08-09 15:30:01 -0300

    feat: websocket impl

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,34 +1,84 @@
 import { ArrowUp } from "lucide-react";
 import { useState } from "react";
+import { createMessageReaction } from "../http/create-message-reaction";
+import { useParams } from "react-router-dom";
+import { toast } from "sonner";
+import { removeMessageReaction } from "../http/remove-message-reaction";
 
 interface MessageProps {
-  text: string
-  amountOfReactions: number
-  answered?: boolean
+  id: string;
+  text: string;
+  amountOfReactions: number;
+  answered?: boolean;
 }
 
-export function Message({ text, amountOfReactions, answered = false }: MessageProps) {
-  const [hasReacted, setHasReacted] = useState(false)
+export function Message({
+  id: messageId,
+  text,
+  amountOfReactions,
+  answered = false,
+}: MessageProps) {
+  const [hasReacted, setHasReacted] = useState(false);
+  const { roomId } = useParams();
 
-  function handleReactToMessage() {
-    setHasReacted(true)
+  if (!roomId) {
+    throw new Error("Messages components must be used within room page");
+  }
+
+  async function removeMessageReactionAction() {
+    if (!roomId) {
+      return;
+    }
+
+    try {
+      await removeMessageReaction({ messageId, roomId });
+    } catch {
+      toast.error('Falha ao remover reação mensagem, tente novamente')
+    }
+
+    setHasReacted(false);
+  }
+
+  async function createMessageReactionAction() {
+    if (!roomId) {
+      return;
+    }
+
+    try {
+      await createMessageReaction({ messageId, roomId });
+    } catch {
+      toast.error('Falha ao reagir a mensagem, tente novamente')
+    }
+
+    setHasReacted(true);
   }
 
   return (
-    <li data-answered={answered} className="ml-4 leading-relaxed text-zinc-100 data-[answered=true]:opacity-50 data-[answered=true]:pointer-events-none">
+    <li
+      data-answered={answered}
+      className="ml-4 leading-relaxed text-zinc-100 data-[answered=true]:opacity-50 data-[answered=true]:pointer-events-none"
+    >
       {text}
 
       {hasReacted ? (
-        <button type="button" className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500">
+        <button
+          onClick={removeMessageReactionAction}
+          type="button"
+          className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500"
+        >
           <ArrowUp className="size-4" />
           Curtir pergunta ({amountOfReactions})
         </button>
       ) : (
-        <button onClick={handleReactToMessage} type="button" className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300">
+        <button
+          onClick={createMessageReactionAction}
+          type="button"
+          className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300"
+        >
           <ArrowUp className="size-4" />
           Curtir pergunta ({amountOfReactions})
         </button>
       )}
     </li>
-  )
+  );
 }
